0459c54880a97f6d2c7d397a850756e240bf7b55
ndsl7109256@gmail.com <ndsl7109256@gmail.com>
2024-10-23 14:23:43 +0800

    good vnc view

--- backend/vnc.c
+++ backend/vnc.c
@@ -17,8 +17,10 @@ All rights reserved.
 #define SCREEN(x) ((twin_context_t *) x)->screen
 #define PRIV(x) ((twin_vnc_t *) ((twin_context_t *) x)->priv)
 
-#ifndef DRM_FORMAT_XRGB8888
-#define DRM_FORMAT_XRGB8888 0x34325258
+#ifndef DRM_FORMAT_ARGB8888
+#define fourcc_code(a, b, c, d) ((uint32_t)(a) | ((uint32_t)(b) << 8) | \
+                 ((uint32_t)(c) << 16) | ((uint32_t)(d) << 24))
+#define DRM_FORMAT_ARGB8888  fourcc_code('A', 'R', '2', '4') 
 #endif
 
 typedef struct {
@@ -34,6 +36,10 @@ typedef struct {
     int width;
     int height;
 } twin_vnc_t;
+
+struct fb_side_data {
+	struct pixman_region16 damage;
+};
 
 static void _twin_vnc_put_begin(twin_coord_t left, twin_coord_t top,
     twin_coord_t right, twin_coord_t bottom, void *closure)
@@ -74,8 +80,12 @@ static void _twin_vnc_put_span(twin_coord_t left,
 
     nvnc_fb_unref(fb);
     */
-
+    /*
     twin_vnc_t *tx = PRIV(closure);
+    struct nvnc_fb *fb;
+    fb = nvnc_fb_pool_acquire(tx->fb_pool);
+    assert(fb);
+
     uint32_t *fb_pixels = tx->framebuffer + top * tx->width + left;
     memcpy(fb_pixels, pixels, (right - left) * sizeof(uint32_t));
 
@@ -83,6 +93,24 @@ static void _twin_vnc_put_span(twin_coord_t left,
     pixman_region_init_rect(&span_region, left, top, right - left, 1);
     pixman_region_union(&tx->damage_region, &tx->damage_region, &span_region);
     pixman_region_fini(&span_region);
+    */
+
+    twin_vnc_t *tx = PRIV(closure);
+    uint32_t *fb_pixels = tx->framebuffer + top * tx->width + left;
+    size_t span_width = right - left;
+    //printf("%d %d %d\n", top, left, right);
+
+    memcpy(fb_pixels, pixels, span_width * sizeof(uint32_t));
+
+    pixman_region_init_rect(&tx->damage_region, 
+                             left, top, span_width, 1);
+
+    if (pixman_region_not_empty(&tx->damage_region)) {
+        nvnc_display_feed_buffer(tx->display, tx->current_fb, &tx->damage_region);
+        pixman_region_clear(&tx->damage_region);
+    }
+    aml_poll(tx->aml, 0);
+    aml_dispatch(tx->aml);
 }
 
 static bool twin_vnc_work(void *closure)
@@ -92,13 +120,14 @@ static bool twin_vnc_work(void *closure)
 
     if (twin_screen_damaged(screen))
         twin_screen_update(screen);
+    /*
     aml_poll(tx->aml, 0);
     aml_dispatch(tx->aml);
     if (pixman_region_not_empty(&tx->damage_region)) {
         nvnc_display_feed_buffer(tx->display, tx->current_fb, &tx->damage_region);
         pixman_region_clear(&tx->damage_region);
     }
-
+    */
     return true;
 }
 
@@ -113,7 +142,7 @@ static void vnc_pointer_event(struct nvnc_client *client,
     event.u.pointer.screen_y = y;
     event.u.pointer.button = button_mask;
 
-    twin_screen_dispatch(tx->screen, &event);
+    //twin_screen_dispatch(tx->screen, &event);
 }
 
 static void vnc_key_event(struct nvnc_client *client,
@@ -125,7 +154,7 @@ static void vnc_key_event(struct nvnc_client *client,
     event.kind = is_pressed ? TwinEventKeyDown : TwinEventKeyUp;
     event.u.key.key = keysym;
 
-    twin_screen_dispatch(tx->screen, &event);
+    //twin_screen_dispatch(tx->screen, &event);
 }
 
 static void vnc_new_client(struct nvnc_client *client)
@@ -141,7 +170,7 @@ static bool twin_vnc_read_events(int fd, twin_file_op_t op, void *closure)
     (void)fd;
     (void)op;
     twin_vnc_t *tx = closure;
-    aml_dispatch(tx->aml);
+    //aml_dispatch(tx->aml);
     return true;
 }
 
@@ -173,7 +202,7 @@ twin_context_t *twin_vnc_init(int width, int height)
     if (!tx->display)
         goto bail_server;
 
-    tx->fb_pool = nvnc_fb_pool_new(width, height, DRM_FORMAT_XRGB8888, 4);
+    tx->fb_pool = nvnc_fb_pool_new(width, height, DRM_FORMAT_ARGB8888, width);
     if (!tx->fb_pool)
         goto bail_display;
 
@@ -191,20 +220,22 @@ twin_context_t *twin_vnc_init(int width, int height)
     if (!ctx->screen)
         goto bail_fb_pool;
 
-    tx->framebuffer = malloc(width * height * sizeof(uint32_t) * 4);
+    tx->framebuffer = malloc(width * height * sizeof(uint32_t));
     if (!tx->framebuffer)
         goto bail_screen;
-    memset(tx->framebuffer, 0, width * height * sizeof(uint32_t) * 4);
+    memset(tx->framebuffer, 0xff, width * height * sizeof(uint32_t));
 
     tx->current_fb = nvnc_fb_from_buffer(tx->framebuffer, width, height,
-        DRM_FORMAT_XRGB8888, width * 4);
+        DRM_FORMAT_ARGB8888, width );
     if (!tx->current_fb)
         goto bail_framebuffer;
 
-    pixman_region_init(&tx->damage_region);
+    pixman_region_init_rect(&tx->damage_region, 
+                            0, 0,
+                            width, height);
 
     int aml_fd = aml_get_fd(tx->aml);
-    tx->aml_handler = aml_handler_new(aml_get_fd(tx->aml), twin_vnc_work, ctx, NULL);
+    //tx->aml_handler = aml_handler_new(aml_get_fd(tx->aml), twin_vnc_work, ctx, NULL);
     twin_set_file(twin_vnc_read_events, aml_fd, TWIN_READ, tx);
 
     twin_set_work(twin_vnc_work, TWIN_WORK_REDISPLAY, ctx);
